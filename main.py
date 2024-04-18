a38c1ef2d9e8aa84fd5755e35d66809e74a22dfb
amitB034 <amit.bhan0223@gmail.com>
2024-04-18 23:40:57 +0900

    2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def sum_receipt():
     result= 0
     N = int(input("枚数は？"))
+    print(N)
     for i in range(N):
         J = int(input(str(i+1)+"枚目の金額は？"))
+        print(J)
         result += J
     
     print("合計は" + str(result) + "円")
