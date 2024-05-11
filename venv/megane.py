08fb2a28edef5419de2d7d9447bfbd38433b6b93
amitB034 <amit.bhan0223@gmail.com>
2024-05-11 12:42:45 +0900

    mecab

--- venv/megane.py
+++ venv/megane.py
@@ -2,22 +2,17 @@
 from pic import Show_image
 
 def main():
-    # Show_imageクラスのインスタンスを作成
     show_image = Show_image()
-    # 画像を選択し、選択されたファイルのパスを取得
     file_path = show_image.circle_pic()
     
     if not file_path:
         print("ファイルが選択されていません。")
         return
 
-    # 貼り付ける画像のパス
     overlay_path = '/Users/amit/Downloads/megane.png'
 
-    # 画像に顔を認識し、貼り付ける
     show_image.paste_image(overlay_path)
-
-    # 結果を表示
+    print(5)
     show_image.show_image()
 
 if __name__ == "__main__":
--- venv/pic.py
+++ venv/pic.py
@@ -17,16 +17,13 @@ class Show_image():
         self.faces = []
 
     def circle_pic(self):
-        # ファイル選択ダイアログを表示して画像ファイルを選択
         file_path = filedialog.askopenfilename(title="画像ファイルを選択",
                                                filetypes=[("PNG files", "*.png"), ("JPEG files", "*.jpeg"), ("All files", "*.*")])
         if not file_path:
             return None
-        # OpenCVで画像を読み込む
+        
         self.image = cv2.imread(file_path)
-        # OpenCVの顔検出用の事前学習済み分類器のパス
         cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-        # 事前学習済みの顔検出器を読み込む
         face_cascade = cv2.CascadeClassifier(cascade_path)
         gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
@@ -43,8 +40,12 @@ class Show_image():
                 for j in range(overlay_image.shape[1]):
                     if overlay_image[i, j][3] != 0:
                         roi[i, j] = overlay_image[i, j][:3]
+        print(4)
 
     def show_image(self):
+        print("finish")
         cv2.imshow("Result", self.image)
+        print("f1")
         cv2.waitKey(0)
+        print("f2")
         cv2.destroyAllWindows()
